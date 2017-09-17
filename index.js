eeccc6ff6d148d2285623a59fb6cc86392ed81dc
Eric Cao <spacewoox@gmail.com>
2017-09-17 22:36:14 +0200

    remove unecessary dependencies

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ import Recos from './src/widgets/Recos';
 import Earlybirds from './src/containers/Earlybirds';
 import Router from './src/containers/Router';
 import Route from './src/containers/Route';
-import TrackActivity from './src/containers/TrackActivity';
-import { Test } from './src/connectors';
 
 module.exports = {
   RecosWidget,
   Earlybirds,
-  TrackActivity,
   Router,
   Route,
-  Test,
   widgets: {
     Recos,
   }
