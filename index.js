baca0f5d73f380e8955ef37e144f3566a70d6d98
Eric Cao <spacewoox@gmail.com>
2017-11-22 14:56:32 +0100

    remove redux code

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-import { h, render, Component } from 'preact';
-import { Slider } from './src/components/Slider';
-import { Render } from './src/components/Render';
-import { Recos } from './src/components/Recos';
+import Slider from './src/components/Slider';
+import Render from './src/components/Render';
+import Recos from './src/components/Recos';
 import { ExpectChild } from './src/hoc/ExpectChild';
 import { CloneElement } from './src/lib/CloneElement';
 
-let config = {
+const config = {
   Slider,
   Render,
   Recos,
   ExpectChild,
-  CloneElement
-}
-/* REDUX-RELATED-START */
-import CreateStore from './src/widgets/CreateStore';
-import { reducerIdentify, reducerGetRecos } from './src/redux/reducers';
-import { getRecommendations } from './src/redux/actions';
+  CloneElement,
+};
 
-config = {
-  ...config,
-  CreateStore,
-  getRecommendations,
-  Reducers: {
-    reducerIdentify,
-    reducerGetRecos
-  }
-}
-/* REDUX-RELATED-END */
 export default config;
