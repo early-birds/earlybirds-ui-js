1fae795b404095c2d12a9995a9bdb4d662293f8b
Eric Cao <spacewoox@gmail.com>
2017-11-22 14:59:27 +0100

    linter compliance

--- src/components/Render.js
+++ src/components/Render.js
@@ -1,23 +1,24 @@
-import { h, render, Component } from 'preact';
-import { CloneElement } from '../lib/CloneElement';
+import { h, render } from 'preact';
 
-export const Render = props => {
+const Render = (props) => {
   const { path, children, type = 'replace' } = props;
   if (path == null) return null;
-  const el = document.querySelector(path)
+  const el = document.querySelector(path);
   let replaceAt;
   if (el) {
-    if (type === "prepend") {
-      el.insertBefore(document.createElement('div'), el.firstElementChild)
+    if (type === 'prepend') {
+      el.insertBefore(document.createElement('div'), el.firstElementChild);
       replaceAt = el.firstElementChild;
-    }
-    else if (type === 'replace') {
+    } else if (type === 'replace') {
       el.innerHTML = '';
       replaceAt = el;
     }
     render(
       <span>{children}</span>,
-      replaceAt
-    )
+      replaceAt,
+    );
   }
-}
+  return null;
+};
+
+export default Render;
