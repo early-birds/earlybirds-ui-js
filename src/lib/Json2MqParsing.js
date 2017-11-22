e0d8400988427ef9ef6e93b7de03ca9f84997a34
Eric Cao <spacewoox@gmail.com>
2017-11-22 14:59:50 +0100

    create separate module for better unit testing

---
+++ src/lib/Json2MqParsing.js
@@ -0,0 +1,16 @@
+import json2mq from 'json2mq'
+
+export const generateSortedMediaQueriesFromList = list => {
+  let lastBp = 0
+  return list
+    .sort((a, b) => a.bp - b.bp)
+    .map(x => {
+      const mq = json2mq({ minWidth: lastBp, maxWidth: x.bp})
+      lastBp = x.bp
+      return {
+        ...x,
+        computed: mq
+      }
+    })
+}
+
