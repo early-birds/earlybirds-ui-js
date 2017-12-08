f11fc13440d8eda87bb57cdeac03dc834deaadd5
jeromewir <jeromwir@gmail.com>
2017-12-08 18:53:44 +0100

    Updated append & cie

--- src/components/Render.js
+++ src/components/Render.js
@@ -1,32 +1,70 @@
 import { h, render, Component } from 'preact';
 
+const insertElem = (element, parent, type) => {
+  const newDiv = document.createElement(element.nodeName);
+  let newEl;
+
+  if (type === 'append') {
+    newEl = parent.appendChild(newDiv);
+  } else if (type === 'prepend') {
+    newEl = parent.insertBefore(newDiv, parent.firstChild);
+  }
+  render(element, null, newEl);
+};
+
 class Render extends Component {
+  static createElement({ path, type, nodeName }) {
+    const parent = document.querySelector(path);
+    const newDiv = document.createElement(nodeName);
+
+    if (!parent) {
+      return null;
+    }
+
+    switch (type) {
+      case 'html':
+        return parent;
+      case 'append':
+        return parent.appendChild(newDiv);
+      case 'prepend':
+        return parent.insertBefore(newDiv, parent.firstChild);
+      case 'before':
+        return parent.parentNode.insertBefore(newDiv, parent);
+      case 'after':
+        return parent.parentNode.insertBefore(newDiv, parent.nextSibling);
+      default:
+        throw new Error('Unhandled render type');
+    }
+  }
 
   componentWillMount() {
     const { path, children, type = 'html' } = this.props;
     if (path == null) return null;
-
     const el = document.querySelector(path);
+    let newDiv;
+
     if (el) {
-      const toBeRendered = <span>{children}</span>;
+      const toBeRendered = children.length > 1 ? <span>{children}</span> : children[0];
 
-      children.forEach((c) => {
-        const newEl = document.querySelector(path).insertBefore(document.createElement('li'), el.firstChild);
-
-        switch (type) {
-          case 'html':
-            render(c, null, newEl);
-            break;
-          case 'append':
-            render(c, newEl);
-            break;
-          case 'prepend':
-            render(c, newEl, newEl.firstChild);
-            break;
-        }
-      });
+      switch (type) {
+        case 'html':
+          render(toBeRendered, null, el);
+          break;
+        case 'append':
+        case 'prepend':
+          children.forEach(child => insertElem(child, el, type));
+          break;
+        case 'before':
+        case 'after':
+          newDiv = document.createElement(toBeRendered.nodeName);
+          el.parentNode.insertBefore(newDiv, type === 'before' ? el : el.nextSibling);
+          render(toBeRendered, null, newDiv);
+          break;
+        default:
+          throw new Error('Unhandled render type');
+      }
     }
   }
-};
+}
 
 export default Render;
